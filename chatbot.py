a1bd2a51d7aae7ff47f92d821effc2b9c739b173
Alfredo Regner <94856772+alfredoregner@users.noreply.github.com>
2025-04-04 16:49:43 -0300

    Ajustes no chatbot
    Coleta de dados para criação de funcionários na planilha

--- chatbot.py
+++ chatbot.py
@@ -7,6 +7,7 @@ print('Digite o número correspondente ao atendimento que deseja: ')
 print('1- Cadastro de Funcionário \n2- Edição de Cadastro \n3- Encerrar atendimento')
 atendimento = int(input('Insira sua opção: '))
 
+# Acessa a planilha com os dados dos funcionários
 planilha = openpyxl.load_workbook('Funcionarios.xlsx')
 pagina = planilha['Funcionarios']
 
@@ -16,18 +17,54 @@ while atendimento < 1 and atendimento > 3:
     atendimento = int(input('Insira sua opção: '))
 
 if atendimento == 1:
-    pass
+    # Coleta os dados que serão inseridos na planilha
+    cadasto_nome = input('Insira o nome do funcionário: ')
+    cadasto_cpf = input('Insira o CPF do funcionário: ')
+    cadasto_email = input('Insira o E-mail do funcionário: ')
+    cadasto_telefone = input('Insira o Telefone do funcionário: ')
+    cadasto_cep = input('Insira o CEP do endereço do funcionário: ')
+    cadasto_rua = input('Insira a Rua do endereço do funcionário: ')
+    cadasto_numero = input('Insira o Número do endereço do funcionário: ')
+    cadasto_complemento = input('Insira o Complemento do endereço do funcionário: ')
+    cadasto_bairro = input('Insira o Bairro do endereço do funcionário: ')
+    cadasto_cidade = input('Insira a Cidade do endereço do funcionário: ')
+    cadasto_estado = input('Insira o Estado do endereço do funcionário: ')
+    cadasto_cargo = input('Insira o Cargo do funcionário: ')
+    cadasto_salario = input('Insira o Salário do funcionário: ')
 
-
-
+    # Insere os dados na planilha
+    pagina.append([cadasto_nome, cadasto_cpf, cadasto_email, cadasto_telefone, cadasto_cep, cadasto_rua, cadasto_numero, cadasto_complemento, cadasto_bairro, cadasto_cidade, cadasto_estado, cadasto_cargo, cadasto_salario])
+    planilha.save('Funcionarios.xlsx') # Salva a planilha com os dados atualizados
 
 
 elif atendimento == 2:
-    workbook = openpyxl.load_workbook('Funcionarios.xlsx')
-    pagina = workbook['Funcionarios']
+    # Inserir o CPF do cadastro desejado para alteração
+    # Verificar se o CPF é valido, e acessar a linha de cadastro desse funcionário
+    # Realizar a alteração de todos os campos de cadastro do usuário (ou talvez separar por tópico: Pessoal, Endereço, Função)
+    for rows in pagina.iter_rows(min_row=2, max_row=None):
+    # Coleta os dados que serão inseridos na planilha
+        edicao_nome = input('Insira o nome do funcionário: ')
+        edicao_cpf = input('Insira o nome do funcionário: ')
+        edicao_email = input('Insira o nome do funcionário: ')
+        edicao_telefone = input('Insira o nome do funcionário: ')
+        edicao_cep = input('Insira o nome do funcionário: ')
+        edicao_rua = input('Insira o nome do funcionário: ')
+        edicao_numero = input('Insira o nome do funcionário: ')
+        edicao_complemento = input('Insira o nome do funcionário: ')
+        edicao_bairro = input('Insira o nome do funcionário: ')
+        edicao_cidade = input('Insira o nome do funcionário: ')
+        edicao_estado = input('Insira o nome do funcionário: ')
+        edicao_cargo = input('Insira o nome do funcionário: ')
+        edicao_salario = input('Insira o nome do funcionário: ')
 
+    # Insere os dados na planilha
+    pagina.append([edicao_nome, edicao_cpf, edicao_email, edicao_telefone, edicao_cep, edicao_rua, edicao_numero, edicao_complemento, edicao_bairro, edicao_cidade, edicao_estado, edicao_cargo, edicao_salario])
+    planilha.save('Funcionarios.xlsx') # Salva a planilha com os dados atualizados
 else :
     print('Agradecemos pelo seu contato. Tenha um ótimo dia!')
+
+
+
 
 
 
